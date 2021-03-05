91d15f77fbefb4dda5cd15d207cb513a8e9deaa5
yongshengdev <yongshengdev@163.com>
2021-03-05 18:38:29 +0800

    init

---
+++ share/src/main/java/com/sign/share/ShareActivity.kt
@@ -0,0 +1,11 @@
+package com.sign.share
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ShareActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_share)
+    }
+}
